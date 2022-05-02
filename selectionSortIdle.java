ac14e739442964c154f39b9e0953ad522a627d5d
Jayallstars <jayza0072555@gmail.com>
2022-05-02 16:36:39 +0700

    commit

--- selectionSortIdle.java
+++ selectionSortIdle.java
@@ -1,20 +1,25 @@
 public class selectionSortIdle {
         public static void selectionSort(int[] arr) {
-                for (int i = 0; i < arr.length - 1; i++) {
-                        int index = i;
-                        for (int j = i + 1; j < arr.length; j++) {
-                                if (arr[j] < arr[index]) {
-                                        index = j;// searching for lowest index
+                int i,j,minValue,minIndex,temp = 0;
+                for (i = 0; i < arr.length; i++){
+                        minValue = arr[i];
+                        minIndex = i;
+                        for (j = i; j < arr.length; j++){
+                                if (arr[j] < minValue){
+                                        minValue = arr[j];
+                                        minIndex = j;
                                 }
                         }
-                        int smallerNumber = arr[index];
-                        arr[index] = arr[i];
-                        arr[i] = smallerNumber;
+                        if (minValue < arr[i]){
+                                temp = arr[i];
+                                arr[i] = arr[minIndex];
+                                arr[minIndex] = temp;
+                        }
                 }
         }
 
         public static void main(String a[]) {
-                int[] arr1 = { 50, 15, 4, 9, 3, 91, 8, 2, 45, 66, 23 };
+                int[] arr1 = { 50, 15, 4, 9, 3, 91 };
                 System.out.println("Before Selection Sort");
                 for (int i : arr1) {
                         System.out.print(i + " ");
